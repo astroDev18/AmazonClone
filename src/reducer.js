a1b77ab557eeee30fb20611d10ff69e0bad455c0
astro_dev <isajaffarali@gmail.com>
2020-12-10 11:50:30 +0300

    Added delete functionality and display items in cart

--- src/reducer.js
+++ src/reducer.js
@@ -15,9 +15,31 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item],
             };
 
+
+        case "REMOVE_FROM_BASKET":
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            // Copy items into new basket
+            let newBasket = [...state.basket];
+            // Go to new basket and splice at point index and remove the item
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+
+            } else {
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as its not in basket!`
+                )
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
-}
+};
 
 export default reducer;
